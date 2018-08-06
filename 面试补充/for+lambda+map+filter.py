b1d9b18b3aec45e7cf05438d2247a246273eb0a3
boke-moban <32098764+sqhl@users.noreply.github.com>
2018-08-06 13:08:54 +0800

    临时察觉

--- 面试补充/for+lambda+map+filter.py
+++ 面试补充/for+lambda+map+filter.py
@@ -7,3 +7,8 @@ print(len(a[0]))
 print([[r[i] for r in a] for i in range(len(a[0]))])
 print("###################")
 print([[1] for i in range(3)])
+#可能时闭包原理
+lst = [lambda x:x*i for i in range(4)]
+print([m(2) for m in lst]) #这个i是全局的，lambda里面保留的引用
+
+#[6, 6, 6, 6]
