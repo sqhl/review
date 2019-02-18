49637e266ac137d8310547283aac5981bae11022
boke-moban <32098764+sqhl@users.noreply.github.com>
2019-02-18 09:01:55 +0800

    Add files via upload

---
+++ 坑向/range的加深理解.py
@@ -0,0 +1,14 @@
+x = 4
+
+# for i in range(x):
+#     print(i, x)
+#     x = 2
+# print(x)
+# print("------------")
+
+
+for i in range(x):
+    for j in range(x):
+        print(j, x)
+        x = 2
+# range(2) 返回一个对象 当每次range完了之后 会重新创建对象
